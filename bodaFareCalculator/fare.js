61be1034d7b1c5914e21e6334088a8f0cdec4f17
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-08 13:36:38 +0300

    Add Boda Boda ride fare estimator script

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,8 @@
+function calculateBodaFare() {
+    const distanceInKm = Number(prompt("Enter the distance in kilometers:"));
+    const baseFare = 50; // Base fare in shillings
+    const chargePerKm = 15; // Fare per kilometer in shillings
+    const totalFare = baseFare + (chargePerKm * distanceInKm);
+
+    console.log(`Total estimated boda boda fare for ${distanceInKm} km is KES ${totalFare}.`);
+}
