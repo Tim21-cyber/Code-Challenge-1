206c6a3109f05bbf97f4eb44c70a192f5cfb3119
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-08 14:08:56 +0300

    Add Chai Bora Ingredient Calculator script

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -4,9 +4,10 @@ function calculateChaiIngredients() {
     const teaLeaves = 1 * numberOfCups; // 1 tsp of tea leaves per cup
     const sugar = 2 * numberOfCups; // 2 tsp of sugar per cup
 
-    console.log(`To make ${numberOfCups} cups of chai, you need:`);
+    console.log(`To make ${numberOfCups} cup${numberOfCups !== 1 ? 's' : ''} of chai, you will need:`);
     console.log(`${water} ml of water`);
     console.log(`${milk} ml of milk`);
-    console.log(`${teaLeaves} tsp of tea leaves`);
-    console.log(`${sugar} tsp of sugar`);
+    console.log(`Tea leaves: ${teaLeaves} tsp${teaLeaves !== 1 ? 's' : ''}`);
+    console.log(`Sugar: ${sugar} tsp${sugar !== 1 ? 's' : ''}`);
+    console.log("\nEnjoy your chai Bora!");
 }
